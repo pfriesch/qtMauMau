f1aa10c6309e631809ad2f0b8c81ec57ecc9741c
Tobi K <t.klasse@googlemail.com>
2014-04-15 17:56:19 +0200

    added logging in main

--- main.cpp
+++ main.cpp
@@ -1,36 +1,45 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include "network/server.h"
-#include "settings.h"
+#include <QtDebug>
+#include <QFile>
+#include <QTextStream>
 
-#include <stdlib.h>
-#include <QVariant>
-#include <stdio.h>
-#include <iostream>
-#include <string>
-#include <network/client.h>
-using namespace std;
+void customMessageHandler(QtMsgType type, const QMessageLogContext& context, const QString &message)
+{
+    QString txt;
+    switch (type) {
+    case QtDebugMsg:
+        txt = QString("Debug: %1").arg(message);
+        break;
+
+    case QtWarningMsg:
+        txt = QString("Warning: %1").arg(message);
+    break;
+    case QtCriticalMsg:
+        txt = QString("Critical: %1").arg(message);
+    break;
+    case QtFatalMsg:
+        txt = QString("Fatal: %1").arg(message);
+        abort();
+    }
+
+    QFile outFile("maumau-log.txt");
+    outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+    QTextStream ts(&outFile);
+    ts << txt << endl;
+}
+
+
 int main(int argc, char *argv[])
 {
 
       QApplication a(argc, argv);
-     QTextStream qtin(stdin);
-      QString word;
 
-      std::cout << "So, what do you wanna?" << std::endl;
-      qtin >> word;
-      if(!word.compare("s")){
-          std::cout << "Server" << std::endl;
-          Server server();
-      }
-      if(!word.compare("c")){
-          std::cout << "Client" << std::endl;
-          Client client();
-      }
-      std::cout << "Bye" << std::endl;
+      //Lets register our custom debug handler, before we start
+      qInstallMessageHandler(customMessageHandler);
 
       MainWindow w;
       w.show();
 
-     return a.exec();
+      return a.exec();
 }
