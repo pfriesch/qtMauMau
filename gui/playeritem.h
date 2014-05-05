bc9bb3b7a7552e7a72bd8e7185a3b2602a455c47
Tobi K <t.klasse@googlemail.com>
2014-05-05 18:34:05 +0200

    all players are now visible with the number cards

--- gui/playeritem.h
+++ gui/playeritem.h
@@ -3,6 +3,7 @@
 
 #include <gui/carditem.h>
 #include <QDebug>
+#include <QString>
 
 class PlayerItem : public QObject {
     Q_OBJECT
@@ -11,21 +12,27 @@ public:
         LEFT = 1,
         TOP = 2,
         RIGHT = 3,
+        BOTTOM = 4
     };
 
     PlayerItem(direction dir, short cardCount, QPointF centerPoint, QObject* parent = 0);
-    QVector<CardItem*> getCards();
-
+    PlayerItem(direction dir, QVector<Card> *humanCards, QPointF centerPoint);
+    QVector<CardItem*>* getCards();
+    void createCards(short cardCount);
 private:
     void measureLayout(short cardCount);
-    QVector<CardItem*> cards;
+    void createHumanCards(QVector<Card> *humanCards);
+    QVector<CardItem*> *cards = new QVector<CardItem*>();
     CardItem::specialCards specialCard;
-    int cardWidth;
-    int cardHeight;
-    int horizontalCardGap = 15;
-    int verticalCardGap = 20;
+    PlayerItem::direction playerDirection;
+    QString layoutKey;
     QHash<QString, int> layout;
     QPointF centerPoint;
+
+    //TODO: set in config.ini not in the code
+    int cardWidth = 71;
+    int cardHeight = 96;
+    int cardGap = 20;
 
 signals:
 
