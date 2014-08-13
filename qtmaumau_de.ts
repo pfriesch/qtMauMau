e49287b27e2a0b7723b4bffa8dbfda86d2be545c
Tobi K <t.klasse@googlemail.com>
2014-08-13 13:23:53 +0200

    added translation english -> german, had to change some labels size

--- qtmaumau_de.ts
+++ qtmaumau_de.ts
@@ -2,62 +2,415 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
 <context>
+    <name>ChooseColorDialog</name>
+    <message>
+        <location filename="gui/choosecolordialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="gui/choosecolordialog.ui" line="110"/>
+        <source>What color do you wish?</source>
+        <translation>Was für eine Farbe wünscht du dir?</translation>
+    </message>
+</context>
+<context>
+    <name>ConnectToServer</name>
+    <message>
+        <location filename="gui/connecttoserver.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="gui/connecttoserver.ui" line="49"/>
+        <source>Connect</source>
+        <translation>verbinden</translation>
+    </message>
+    <message>
+        <location filename="gui/connecttoserver.ui" line="71"/>
+        <source>IP-Address:</source>
+        <translation>IP-Adresse:</translation>
+    </message>
+    <message>
+        <location filename="gui/connecttoserver.ui" line="84"/>
+        <source>Port:</source>
+        <translation>Port:</translation>
+    </message>
+    <message>
+        <location filename="gui/connecttoserver.ui" line="97"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <location filename="gui/connecttoserver.ui" line="117"/>
+        <source>Connect to Server</source>
+        <translation>verbinde zu Server</translation>
+    </message>
+</context>
+<context>
+    <name>CreateServerDialog</name>
+    <message>
+        <location filename="gui/createserverdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="gui/createserverdialog.ui" line="33"/>
+        <source>Server</source>
+        <translation>Server</translation>
+    </message>
+    <message>
+        <location filename="gui/createserverdialog.ui" line="46"/>
+        <source>Player 1</source>
+        <translation>Spieler 1</translation>
+    </message>
+    <message>
+        <location filename="gui/createserverdialog.ui" line="59"/>
+        <source>Player 2</source>
+        <translation>Spieler 2</translation>
+    </message>
+    <message>
+        <location filename="gui/createserverdialog.ui" line="72"/>
+        <source>Player 3</source>
+        <translation>Spieler 3</translation>
+    </message>
+    <message>
+        <location filename="gui/createserverdialog.ui" line="85"/>
+        <source>Start Game</source>
+        <translation>Spiel starten</translation>
+    </message>
+    <message>
+        <location filename="gui/createserverdialog.ui" line="98"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <location filename="gui/createserverdialog.ui" line="111"/>
+        <source>IP-Adress:</source>
+        <translation>IP-Adresse:</translation>
+    </message>
+    <message>
+        <location filename="gui/createserverdialog.ui" line="124"/>
+        <source>Port:</source>
+        <translation>Port:</translation>
+    </message>
+    <message>
+        <location filename="gui/createserverdialog.ui" line="137"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/createserverdialog.ui" line="163"/>
+        <source>Add Computer</source>
+        <translation>füge Computer hinzu</translation>
+    </message>
+    <message>
+        <location filename="gui/createserverdialog.ui" line="182"/>
+        <location filename="gui/createserverdialog.ui" line="201"/>
+        <location filename="gui/createserverdialog.ui" line="220"/>
+        <source>No Player</source>
+        <translation>Kein Spieler</translation>
+    </message>
+</context>
+<context>
+    <name>InfoDialog</name>
+    <message>
+        <location filename="gui/infodialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="gui/infodialog.ui" line="42"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt;&quot;&gt;Hier kommen die Lizenz und so zeug rein blalblablalb&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/infodialog.ui" line="60"/>
+        <source>Twitter about it!</source>
+        <translation>Twitter es doch!</translation>
+    </message>
+</context>
+<context>
     <name>OptionDialog</name>
     <message>
-        <location filename="../optiondialog.ui" line="14"/>
-        <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <location filename="gui/optiondialog.ui" line="17"/>
+        <source>Options</source>
+        <translation>Optionen</translation>
     </message>
     <message>
-        <location filename="../optiondialog.ui" line="48"/>
+        <source>Allgemein</source>
+        <translation type="vanished">Allgemein</translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="61"/>
+        <source>Port:</source>
+        <translation>Port:</translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="74"/>
         <source>Player:</source>
-        <translation type="unfinished"></translation>
+        <translation>Spieler: </translation>
     </message>
     <message>
-        <location filename="../optiondialog.ui" line="71"/>
+        <location filename="gui/optiondialog.ui" line="87"/>
         <source>Language:</source>
+        <translation>Sprache:</translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="133"/>
+        <source>Volume:</source>
+        <translation>Lautstärke:</translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="194"/>
+        <source>Music:</source>
+        <translation>Musik:</translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="200"/>
+        <source>Decks</source>
+        <translation>Decks</translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="250"/>
+        <source>Deck 1</source>
+        <translation>Deck 1</translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="263"/>
+        <source>Deck 2</source>
+        <translation>Deck 2</translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="291"/>
+        <source>Special Cards</source>
+        <translation>Besondere Karten</translation>
+    </message>
+    <message>
+        <source>Ziehe 2 Karten</source>
+        <translation type="vanished">Ziehe 2 Karten</translation>
+    </message>
+    <message>
+        <source>Wünsche Karte</source>
+        <translation type="vanished">Wünsche Karte</translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="33"/>
+        <source>General</source>
+        <translation>Allgemein</translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="308"/>
+        <source>Draw 2 Cards</source>
+        <translation>2 Karten ziehen</translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="326"/>
+        <source>Wish Card</source>
+        <translation>Farbe wünschen</translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="344"/>
+        <source>Change Direction Card</source>
+        <translation>Richtungswechsel Karte</translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="362"/>
+        <source>Skip Player Card</source>
+        <translation>Aussetzen Karte</translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="382"/>
+        <location filename="gui/optiondialog.ui" line="435"/>
+        <location filename="gui/optiondialog.ui" line="488"/>
+        <source>7</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../optiondialog.ui" line="97"/>
-        <source>TextLabel</source>
+        <location filename="gui/optiondialog.ui" line="387"/>
+        <location filename="gui/optiondialog.ui" line="440"/>
+        <location filename="gui/optiondialog.ui" line="493"/>
+        <source>8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="392"/>
+        <location filename="gui/optiondialog.ui" line="445"/>
+        <location filename="gui/optiondialog.ui" line="498"/>
+        <source>9</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="397"/>
+        <location filename="gui/optiondialog.ui" line="450"/>
+        <location filename="gui/optiondialog.ui" line="503"/>
+        <source>10</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="402"/>
+        <location filename="gui/optiondialog.ui" line="455"/>
+        <location filename="gui/optiondialog.ui" line="508"/>
+        <source>Jack</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="407"/>
+        <location filename="gui/optiondialog.ui" line="460"/>
+        <location filename="gui/optiondialog.ui" line="513"/>
+        <source>Queen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="412"/>
+        <location filename="gui/optiondialog.ui" line="465"/>
+        <location filename="gui/optiondialog.ui" line="518"/>
+        <source>King</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="417"/>
+        <location filename="gui/optiondialog.ui" line="470"/>
+        <location filename="gui/optiondialog.ui" line="523"/>
+        <source>Ace</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="544"/>
+        <source>7</source>
+        <comment>1</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="549"/>
+        <source>8</source>
+        <comment>2</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="554"/>
+        <source>9</source>
+        <comment>3</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="559"/>
+        <source>10</source>
+        <comment>4</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="564"/>
+        <source>Jack</source>
+        <comment>5</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="569"/>
+        <source>Queen</source>
+        <comment>6</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="574"/>
+        <source>King</source>
+        <comment>7</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/optiondialog.ui" line="579"/>
+        <source>Ace</source>
+        <comment>8</comment>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>QAction</name>
     <message>
-        <location filename="mainwindow.cpp" line="40"/>
         <source>Start Game</source>
-        <translation>Spiel starten</translation>
+        <translation type="vanished">Spiel starten</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="43"/>
         <source>Options</source>
-        <translation>Optionen</translation>
+        <translation type="vanished">Optionen</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="46"/>
+        <location filename="mainwindow.cpp" line="63"/>
+        <source>Start Local Game</source>
+        <translation>Starte Lokales Spiel</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="67"/>
+        <source>Connect to Server...</source>
+        <translation>verbinde zu Server...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="71"/>
+        <source>Create Server...</source>
+        <translation>Erstelle Server...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="75"/>
+        <source>Options...</source>
+        <translation>Optionen...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="81"/>
         <source>Exit</source>
         <translation>Beenden</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="89"/>
+        <source>About</source>
+        <translation>Über Mau Mau</translation>
     </message>
 </context>
 <context>
     <name>QMenu</name>
     <message>
-        <location filename="mainwindow.cpp" line="37"/>
+        <location filename="mainwindow.cpp" line="60"/>
         <source>File</source>
         <translation>Datei</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="50"/>
+        <location filename="mainwindow.cpp" line="86"/>
         <source>Info</source>
         <translation>Info</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="53"/>
         <source>Playername</source>
-        <translation>Spielername</translation>
+        <translation type="vanished">Spielername</translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <location filename="gui/playground.cpp" line="267"/>
+        <source>Congratulations Player </source>
+        <translation>Herzlichen Glückwunsch Spieler </translation>
+    </message>
+</context>
+<context>
+    <name>SetNameDialog</name>
+    <message>
+        <location filename="gui/setnamedialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="gui/setnamedialog.ui" line="49"/>
+        <source>What&apos;s your name?</source>
+        <translation>Wie ist dein Name?</translation>
+    </message>
+    <message>
+        <location filename="gui/setnamedialog.ui" line="72"/>
+        <source>Name:</source>
+        <translation>Name: </translation>
     </message>
 </context>
 </TS>
